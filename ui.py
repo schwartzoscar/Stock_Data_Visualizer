4310b3506fd8cdc3186502245015b47fe0a2ba41
jermb <77866654+jermb@users.noreply.github.com>
2023-03-09 15:28:30 -0600

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,1 +1,6 @@
 
+# This document contains functions to create the user interface
+
+# It should take input and call the functions in error.py to check if it is valid
+
+# It should then send the user data to the functions in api.py
