0063d86bc94a9654e3c99a8c63f5a03295d955c4
Oscar S <ols6vk@umsystem.edu>
2023-03-16 14:04:42 -0500

    Updated ui.py

--- ui.py
+++ ui.py
@@ -1,3 +1,25 @@
+while True:
+    print("Stock Data Visualizer\n--------------------")
+    Symbol = input("Enter the stock Symbol you are looking for: ")
+    print(Symbol)
+    #CHECK IF SYMBOL IS VALID USING error.py
+    
+    valid_chart_type = False
+    while not valid_chart_type:
+        Chart_type = input("Chart Types\n----------\n1. Bar\n2. Line\nEnter the chart type you want (1, 2): ")
+        if Chart_type == '1':
+            print("Bar")
+            valid_chart_type = True
+        elif Chart_type == '2':
+            print("Line")
+            valid_chart_type = True
+        else:
+            print("Invalid input. Please enter 1 or 2.")
+    
+    restart = input("Do you want to restart the program? (y/n): ")
+    
+    if restart.lower() != 'y':
+        break
 
 # This document contains functions to create the user interface
 
