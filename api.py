861de027ea4c8a8e15692800097c74585fbb8d10
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2023-03-16 14:40:31 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,12 @@
+import requests
 #This file should handle all of the api interactions and be able to return values to other files
-def main():
-    return
-key = "ARJ4YHDD7BSSD94B"
-main()
+#def main():
+    #return
+
+url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=ARJ4YHDD7BSSD94B'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+#key = "ARJ4YHDD7BSSD94B"
+#main()
