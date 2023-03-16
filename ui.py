1f7e71ae244f104625cc1c3bdc9b0ea0d342923c
Oscar S <ols6vk@umsystem.edu>
2023-03-16 14:45:53 -0500

    Split into functions

--- ui.py
+++ ui.py
@@ -1,13 +1,9 @@
-import graph
-import api
-import error
-import app
-while True:
-    print("Stock Data Visualizer\n--------------------")
+def get_stock_symbol():
     stockSymbol = input("Enter the stock Symbol you are looking for: ")
     print(stockSymbol)
-    #CHECK IF SYMBOL IS VALID USING error.py
-    
+    return stockSymbol
+
+def get_chart_type():
     valid_chart_type = False
     while not valid_chart_type:
         chartType = input("Chart Types\n----------\n1. Bar\n2. Line\nEnter the chart type you want (1, 2): ")
@@ -19,32 +15,40 @@ while True:
             valid_chart_type = True
         else:
             print("Invalid input. Please enter 1 or 2.")
+    return chartType
 
+def get_time_series():
     valid_time_type = False
     while not valid_time_type:
         timeSeries = input("Select the time series of the chart you want to generate\n--------------------------\n1. Intradaily\n2. Daily\n3. Weekly\n4. Monthly\nEnter the time series option (1, 2, 3, 4): ")
-        if chartType == '1':
+        if timeSeries == '1':
             print("Intradaily")
             valid_time_type = True
-        elif chartType == '2':
+        elif timeSeries == '2':
             print("Daily")
             valid_time_type = True
-        elif chartType == '3':
+        elif timeSeries == '3':
             print("Daily")
             valid_time_type = True
-        elif chartType == '4':
+        elif timeSeries == '4':
             print("Weekly")
             valid_time_type = True
         else:
             print("Invalid input. Please enter 1, 2, 3 or 4.")
-    
+    return timeSeries
+
+def restart_program():
     restart = input("Do you want to restart the program? (y/n): ")
-    
-    if restart.lower() != 'y':
-        break
+    return restart.lower() == 'y'
 
-# This document contains functions to create the user interface
+def main():
+    while True:
+        print("Stock Data Visualizer\n--------------------")
+        stockSymbol = get_stock_symbol()
+        chartType = get_chart_type()
+        timeSeries = get_time_series()
 
-# It should take input and call the functions in error.py to check if it is valid
+        if not restart_program():
+            break
 
-# It should then send the user data to the functions in api.py
+main()
