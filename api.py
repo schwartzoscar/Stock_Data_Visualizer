eb5a07c22c8b16e19f0457d8c93f0d38847fc752
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2023-03-22 18:27:17 -0500

    Update api.py
    Put all of the code into a function that returns the data values so that they can then be passed through SetStock.

--- api.py
+++ api.py
@@ -5,22 +5,19 @@ import app
 # get the symbol of the stock data the user is wishing to view, before running it through the url variable with retrieves
 # the data from the API.
 
-
-app.SetApi("ARJ4YHDD7BSSD94B")
-
-url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={app.GetStockSymbol()}&apikey={app.GetApi()}"
-r = requests.get(url)
-data = r.json()
-stock_data = data["Global Quote"]
-
-
 # To access one of these variables you will first need to use the following example,
-
 #import app
 #stock_data = app.GetStock()
 #symbol = stock_data["symbol"]
 
-app.SetStock({
+app.SetApi("ARJ4YHDD7BSSD94B")
+
+def pullStock():
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={app.GetStockSymbol()}&apikey={app.GetApi()}"
+    r = requests.get(url)
+    data = r.json()
+    stock_data = data["Global Quote"]
+    return{
     "symbol": stock_data["01. symbol"],
     "open": stock_data["02. open"],
     "high": stock_data["03. high"],
@@ -31,5 +28,8 @@ app.SetStock({
     "previous_close": stock_data["08. previous close"],
     "change": stock_data["09. change"],
     "change_percent": stock_data["10. change percent"]
-})
+
+    }
+
+app.SetStock(pullStock())
 
