97faedca8ff5926583752e14bf953b31ead491f3
cdswpv <cdswpv@umsystem.edu>
2023-03-22 19:10:25 -0500

    Update error.py
    Added error condition

--- error.py
+++ error.py
@@ -1,5 +1,11 @@
 # This file should error check every possible error
-def main():
-    return
-
-main()
+def CheckSymbolInput(x):
+    count = 0
+    for char in x:
+        if count > 5:
+            print("Invalid Input. Too many characters in input, make sure to NOT include $")
+            return False
+        else:
+            count += 1
+            return True
+        
