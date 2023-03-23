c8074141af81af32ee1b77a6a358af3229e29a18
Oscar <89543006+Osczrr@users.noreply.github.com>
2023-03-22 20:26:54 -0500

    Fixed while loop so ui.restart_program() function allows restart

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
 import app, api, ui, graph, error
 
 def main():
-    #Call function in ui.py that begins the interaction between the user and system
-    print("Stock Data Visualizer\n--------------------")
-    app.SetStockSymbol(ui.get_stock_symbol())
-    app.SetChartType(ui.get_chart_type())
-    app.SetTimeSeries(ui.get_time_series())
-    app.SetDates(ui.get_dates())
-    api.pullStock()
-    if (app.GetChartType() == "Bar"): graph.create_bar_graph()
-    else: graph.create_line_graph()
+    while True:
+        #Call function in ui.py that begins the interaction between the user and system
+        print("Stock Data Visualizer\n--------------------")
+        app.SetStockSymbol(ui.get_stock_symbol())
+        app.SetChartType(ui.get_chart_type())
+        app.SetTimeSeries(ui.get_time_series())
+        app.SetDates(ui.get_dates())
+        api.pullStock()
+        if (app.GetChartType() == "Bar"): graph.create_bar_graph()
+        else: graph.create_line_graph()
 
-    if not ui.restart_program():
-            #break
-            return
+        if not ui.restart_program():
+                break
     return
 main()
