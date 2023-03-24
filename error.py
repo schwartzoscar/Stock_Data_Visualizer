c55b5f75de45cf441322043a68fd1c327ce75993
SamuelZZFD <112222031+SamuelZZFD@users.noreply.github.com>
2023-03-23 22:17:01 -0500

    Update error.py

--- error.py
+++ error.py
@@ -3,6 +3,9 @@ def CheckSymbolInput(x):
     count = 0
     for char in x:
         count += 1
+    if not char.isalnum():
+        print("Invalid Input. Input contains special characters.")
+        return False
     if count > 5:
         print("Invalid Input. Too many characters in input, make sure to NOT include $")
         return False
